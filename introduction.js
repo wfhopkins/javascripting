7b97b9ad6c7346e184b4015a0a8ab58540709a13
wfhopkins <will.hop@gmail.com>
2023-04-21 16:11:31 -0700

    Initial commit with all my work so far

---
+++ introduction.js
@@ -0,0 +1,4 @@
+console.log('hello')
+console.log('hello')
+console.log('-----------------')
+console.log('Hello World!')
---
+++ string.js
@@ -0,0 +1,5 @@
+const someString = 'this is a string'
+console.log(someString)
+const example = 'example string'
+example.length
+console.log(example.length)
